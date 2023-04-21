ea770564e6375713b8c9b399d9abe603c3651784
lipem <lipem@MSI>
2023-04-21 19:49:33 +0100

    Agregado time is out

---
+++ src/Proyecto_Ing.java
@@ -0,0 +1,40 @@
+
+public class Proyecto_Ing {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int A=0;
+		do {
+			if (A==0) {
+				System.out.println ("IMPRIME COMO DIOS MANDA");
+				A++;
+			}
+			else {
+				System.out.println ("Se acabó la clase");
+			}	
+		} while (A<2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//Main
+
+}//CLASS
