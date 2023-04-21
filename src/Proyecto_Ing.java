c8a74453eabccbb2aa147f7b5f73bc9f935c5aeb
lipem <lipem@MSI>
2023-04-21 19:52:48 +0100

    Borrado el bucle

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -4,17 +4,7 @@ public class Proyecto_Ing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int A=0;
-		do {
-			if (A==0) {
-				System.out.println ("IMPRIME COMO DIOS MANDA");
-				A++;
-			}
-			else {
-				System.out.println ("Se acabó la clase");
-			}	
-		} while (A<2);
-		
+		System.out.println ("IMPRIME COMO DIOS MANDA");		
 		
 		
 		
