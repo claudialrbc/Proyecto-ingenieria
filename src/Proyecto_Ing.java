8ff5d34ecf72532db4f2f1f470e3552d69f373d3
lipem <lipem@MSI>
2023-04-27 19:27:09 +0100

    Descripción de todas las funciones que tenemos que desarrollar.

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -1,7 +1,7 @@
 
 public class Proyecto_Ing { 
 	
-	//1. Función del valor del Ancho de Banda: 
+	//1. Función del valor del Ancho de Banda (valor entre 20 y 1000 Mbps) 
 	public static String AnchoBanda() {
 		double ValorAnchoBanda = (double)(Math.random()*(1000-20+1));
 		String bandera = "  ";
@@ -15,6 +15,12 @@ public class Proyecto_Ing {
 		}
 		return bandera;
 	}
+	//2. Función del valor del Throughput (valor entre 20 y 1000 Mbps)
+	//3. Función del valor de la Latencia (valor entre 0 y 500 milisegundos)
+	//4. Función del valor de la Tasa de Errores de Bits, BER (valor entre: 10-9%, 10-6%, 10-3%)
+	//5. Función del valor del Tráfico de Datos o Flujo de Datos (valor entre 0 y 1 erlang/horas)
+	
+	
 	
 	public static void main(String[] args) {
 
