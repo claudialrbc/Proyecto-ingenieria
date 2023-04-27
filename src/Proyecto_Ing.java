dd9739f1f8c82754db3ef108e754b34d72b90fde
lipem <lipem@MSI>
2023-04-27 18:07:07 +0100

    Generado y evaluado el número aleatorio respecto al Ancho de Banda.

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -1,13 +1,24 @@
 
-public class Proyecto_Ing {
+public class Proyecto_Ing { 
+	
+	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println ("IMPRIME COMO DIOS MANDA");		
-		
-		
-		
+		//Evaluación del Ancho de Banda
+		double AnchoBanda = (double)(Math.random()*(1000-20+1));
+		String ValorAnchoBanda = " ";
+		//El 90% de este rango serían valores superiores a 890,91
+		//Por lo tanto, evaluamos el aleatorio del Ancho de Banda:
+		if (AnchoBanda>=890.91) {
+			ValorAnchoBanda = "Error en el Ancho de Banda. ";
+			System.out.println (ValorAnchoBanda + "Es: " + AnchoBanda);
+			
+		}
+		else {
+			ValorAnchoBanda = "El Ancho de Banda está en valores correctos. ";
+			System.out.println (ValorAnchoBanda + "Es: " + AnchoBanda);
+		}
 		
 		
 		
