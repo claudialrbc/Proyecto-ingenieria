d65bf06494177b29172d1d9222360c374196051d
lipem <lipem@MSI>
2023-04-27 18:38:18 +0100

    Se ha añadido la función del ancho de banda. Mi idea es eliminar por completo todo del código, y crear todo a través de funciones.

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -1,7 +1,20 @@
 
 public class Proyecto_Ing { 
 	
-	
+	//1. Función del valor del Ancho de Banda: 
+	public static String AnchoBanda() {
+		double ValorAnchoBanda = (double)(Math.random()*(1000-20+1));
+		String bandera = " ";
+		//El 90% de este rango serían valores superiores a 890,91
+		//Por lo tanto, evaluamos el aleatorio del Ancho de Banda:
+		if (ValorAnchoBanda>=890.91) {
+			bandera="¡ALERTA!: El Ancho de Banda ha superado el 90%.";
+		}
+		else {
+			bandera="Ancho de Banda estable";
+		}
+		return bandera;
+	}
 	
 	public static void main(String[] args) {
 
