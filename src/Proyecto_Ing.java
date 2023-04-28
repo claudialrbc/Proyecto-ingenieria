3efe63837891c0180a7c82961c9a0aa8dcd3150f
lipem <lipem@MSI>
2023-04-28 11:10:27 +0100

    Creacción de parámetros aleatorios.

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -1,90 +1,99 @@
 
 public class Proyecto_Ing { 
 	
-	//1. Función del valor del Ancho de Banda (valor entre 20 y 1000 Mbps) 
-/*	public static double AnchoBanda() {
-		double ValorAnchoBanda = (double)(Math.random()*(1000-20+1));
-		String bandera = "  ";
-		//El 90% de este rango serían valores superiores a 890,91
-		//Por lo tanto, evaluamos el aleatorio del Ancho de Banda:
-		if (ValorAnchoBanda>=890.91) {
-			bandera="¡ALERTA!: El Ancho de Banda ha superado el 90%.";
-		}
-		else {
-			bandera="Ancho de Banda estable";
-		}
-		return ValorAnchoBanda;
-	}*/
 	
-	public static double AnchoBanda() {
-		double ValorAnchoBanda = (double)(Math.random()*(1000-20+1));
-		return ValorAnchoBanda;
-	}
-	
-	
-	//2. Función del valor del Throughput (valor entre 20 y 1000 Mbps)
-	public static double Throughput() {
-		double ValorThroughput = (double)(Math.random()*(1000-20+1));
-		return ValorThroughput;
-	}
-	
-	//3. Función del valor de la Latencia (valor entre 0 y 500 milisegundos)
-	public static double Latencia() {
-		double ValorLatencia = (double)(Math.random()*(500-0+1));
-		return ValorLatencia;
-	}
-	
-	//4. Función del valor de la Tasa de Errores de Bits, BER (valor entre: 10-9, 10-6, 10-3). Medido en porcentajes.
-	
-	/* Esto es una movida. 
-	 * Son números elevados a negativo. 
-	 * Ejemplo: 10^-6 => 1.000.000 bits transmitidos, 1 bit tenía error. => 1/10^6 = 10^-6
-	 * Claudia, yo entiendo que a eso se refiere el enunciado, que se refiere a que esos menos son que están elevados.
-	 * 
-	 */
-	
-	
-	//5. Función del valor del Tráfico de Datos o Flujo de Datos (valor entre 0 y 1 erlang/horas)
-	public static double TraficoDatos() {
-		double ValorTraficoDatos = (double)(Math.random()*(1-0+1));
-		return ValorTraficoDatos;
-	}
-	
+	//Función genérica para generar números aleatorios FLOAT (Usada en: Ancho de Banda; Throughput; Latencia):
+		public static float generaNumAleatorioFLOAT (float minimo, float maximo) {
+			return (float)(Math.random()*(minimo-(maximo+1))+(maximo+1));
+		}//llave (aleatorios con decimales)
+		
+	//Función genérica para generar números aleatorios INT (Usada en: Tasa de error de bits/BER ; Tráfico de Datos/Flujo de Datos):
+		public static int generaNumAleatorioINT (int minimo, int maximo) {
+			return (int)(Math.random()*(minimo-(maximo+1))+(maximo+1));
+		}//llave (aleatorios int)
 	
 	
 	public static void main(String[] args) {
 
-		//Evaluación del Ancho de Banda
-		double AnchoBanda = (double)(Math.random()*(1000-20+1));
-		String ValorAnchoBanda = " ";
-		//El 90% de este rango serían valores superiores a 890,91
-		//Por lo tanto, evaluamos el aleatorio del Ancho de Banda:
-		if (AnchoBanda>=890.91) {
-			ValorAnchoBanda = "Error en el Ancho de Banda. ";
-			System.out.println (ValorAnchoBanda + "Es: " + AnchoBanda);
-			
-		}
-		else {
-			ValorAnchoBanda = "El Ancho de Banda está en valores correctos. ";
-			System.out.println (ValorAnchoBanda + "Es: " + AnchoBanda);
-		}
 		
+		////BLOQUE DE CONSTRUCCIÓN DE VALORES////
 		
+		/*/Comprobación de valores. En este bucle se comprueba que el número ni supera 1000,ni es menor que 20. A base de imprimir aleatorios.
+		for (int i=0; i<1000;i++) {
+			double min=20;
+			double max=1000;
+			System.out.println (generaNumAleatorioDOUBLE(min,max));
+		}(llave for)*/
+	
+		//ANCHO DE BANDA
+		float minAnchoBanda=20;
+		float maxAnchoBanda=1000;
+		float valorAnchoBanda=generaNumAleatorioFLOAT(minAnchoBanda,maxAnchoBanda);
+		System.out.println ("El Ancho de Banda es " + valorAnchoBanda + " Mbps.");	
 		
+		//THROUGHPUT
+		float minThroughput=20;
+		float maxThroughput=1000;
+		float valorThroughput=generaNumAleatorioFLOAT(minThroughput,maxThroughput);
+		System.out.println ("El Throughput es " + valorThroughput + " Mbps.");	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}//Main
+		//LATENCIA
+		float minLatencia=0;
+		float maxLatencia=500;
+		float valorLatencia=generaNumAleatorioFLOAT(minLatencia,maxLatencia);
+		System.out.println ("La Latencia es de " + valorLatencia + " miliSegundos(ms).");	
 
-}//CLASS
+		//TASA DE ERROR DE BITS (BER) **************************NO ACABADO, SERIAS DUDAS********************************************
+		int minBER=0;
+		int maxBER=0;
+		int valorBER = generaNumAleatorioINT(minBER,maxBER);
+		System.out.println ("La Tasa de Error de Bits es de " + valorBER + "%.");
+		
+		//TRÁFICO DE DATOS O FLUJO DE DATOS
+		int minFlujoDatos=0;
+		int maxFlujoDatos=1;
+		int valorFlujoDatos = generaNumAleatorioINT(minFlujoDatos,maxFlujoDatos);
+		System.out.println ("El Tráfico de Datos es de " + valorFlujoDatos + " erlong/horas.");
+		
+		
+		////BLOQUE DE EVALUACIÓN DE VALORES////
+		////BLOQUE DE CREACCIÓN DE FICHERO////
+		////BLOQUE DE FILTRADO/ANÁLISIS DE FICHERO////
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//Llave del Main (NO BORRAR)
+
+}//Llave de la Class (NO BORRAR)
