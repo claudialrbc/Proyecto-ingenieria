58bc94a478b03914923df8370ace427c3a7bc735
lipem <lipem@MSI>
2023-04-29 17:36:47 +0100

    Creacción de fichero CSV con los primeros parámetros aleatorios.

--- src/Proyecto_Ing.java
+++ src/Proyecto_Ing.java
@@ -1,6 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
 
-public class Proyecto_Ing { 
-	
+public class Proyecto_Ing {
+
 	
 	//Función genérica para generar números aleatorios FLOAT (Usada en: Ancho de Banda; Throughput; Latencia):
 		public static float generaNumAleatorioFLOAT (float minimo, float maximo) {
@@ -11,50 +13,62 @@ public class Proyecto_Ing {
 		public static int generaNumAleatorioINT (int minimo, int maximo) {
 			return (int)(Math.random()*(minimo-(maximo+1))+(maximo+1));
 		}//llave (aleatorios int)
-	
+		
 	
 	public static void main(String[] args) {
 
 		
-		////BLOQUE DE CONSTRUCCIÓN DE VALORES////
+		////BLOQUE DE CONSTRUCCIÓN DE VALORES ALEATORIOS////
 		
-		/*/Comprobación de valores. En este bucle se comprueba que el número ni supera 1000,ni es menor que 20. A base de imprimir aleatorios.
-		for (int i=0; i<1000;i++) {
-			double min=20;
-			double max=1000;
-			System.out.println (generaNumAleatorioDOUBLE(min,max));
-		}(llave for)*/
-	
 		//ANCHO DE BANDA
 		float minAnchoBanda=20;
 		float maxAnchoBanda=1000;
 		float valorAnchoBanda=generaNumAleatorioFLOAT(minAnchoBanda,maxAnchoBanda);
 		System.out.println ("El Ancho de Banda es " + valorAnchoBanda + " Mbps.");	
-		
 		//THROUGHPUT
 		float minThroughput=20;
 		float maxThroughput=1000;
 		float valorThroughput=generaNumAleatorioFLOAT(minThroughput,maxThroughput);
 		System.out.println ("El Throughput es " + valorThroughput + " Mbps.");	
-		
 		//LATENCIA
 		float minLatencia=0;
 		float maxLatencia=500;
 		float valorLatencia=generaNumAleatorioFLOAT(minLatencia,maxLatencia);
 		System.out.println ("La Latencia es de " + valorLatencia + " miliSegundos(ms).");	
-
 		//TASA DE ERROR DE BITS (BER) **************************NO ACABADO, SERIAS DUDAS********************************************
 		int minBER=0;
 		int maxBER=0;
 		int valorBER = generaNumAleatorioINT(minBER,maxBER);
 		System.out.println ("La Tasa de Error de Bits es de " + valorBER + "%.");
-		
 		//TRÁFICO DE DATOS O FLUJO DE DATOS
 		int minFlujoDatos=0;
 		int maxFlujoDatos=1;
 		int valorFlujoDatos = generaNumAleatorioINT(minFlujoDatos,maxFlujoDatos);
 		System.out.println ("El Tráfico de Datos es de " + valorFlujoDatos + " erlong/horas.");
 		
+		
+		////BLOQUE DE CREACCIÓN DE FICHERO CSV////
+		
+		File fichero = new File ("prueba.csv"); //creacción del fichero con el nombre de "prueba"
+		//Estructura try-catch para asegurar que no hay fallas en la creacción del fichero.
+		try (FileWriter fw = new FileWriter(fichero);) {
+			//Declaramos un Array de nombres para guardarlo en el fichero como cabeceras.
+			String [] cabeceraNombres = {" Ancho de Banda ", " Throughput ", " Latencia ", " BER ", " Flujo de Datos"};
+			//Bucle que escribe las cabeceras:
+			for (int i=0;i<5;i++) { 
+				fw.write(cabeceraNombres [i] + "    ");
+			}
+			fw.write("\n"); //pasamos a la siguiente línea en el fichero.
+			//Declaramos un Array con los elementos generados aleatoriamente.
+			float [] Datos= {valorAnchoBanda, valorThroughput, valorLatencia, valorBER, valorFlujoDatos};
+			//Bucle que escrie los parámetros:
+			for (int i=0; i<5; i++) {
+				fw.write(Datos[i] + "    ");
+			}
+		} catch (Exception e) { 
+			System.out.println ("Se ha producido un error."); 
+		}//fin catch
+	
 		
 		////BLOQUE DE EVALUACIÓN DE VALORES////
 		
@@ -75,9 +89,9 @@ public class Proyecto_Ing {
 		//TASA DE ERROR DE BITS (BER) (variable: valorBER)
 		//TRÁFICO DE DATOS O FLUJO DE DATOS (variable: valorFlujoDatos)
 		
+	
 		
 		
-		////BLOQUE DE CREACCIÓN DE FICHERO////
 		////BLOQUE DE FILTRADO/ANÁLISIS DE FICHERO////
 		
 		
